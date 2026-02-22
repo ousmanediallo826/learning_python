dba620d46fe90ce7a37b8159d182fd48b908c680
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-02-22 16:43:07 -0500

    Developer Fundamnetals II

--- python_basic_I/developer_fundamental_II/main.py
+++ python_basic_I/developer_fundamental_II/main.py
@@ -1,8 +1,33 @@
 #password Checkr
 
-user_name = input("Please enter your name: ")
-password = input("Please enter your password: ")
+#user_name = input("Please enter your name: ")
+#password = input("Please enter your password: ")
 
-encrypted_password = ("*" * len(password))
+#encrypted_password = ("*" * len(password))
 
-print(f"Hi, {user_name}, your password is {encrypted_password} ans it is {len(password)} digits long.")
+#print(f"Hi, {user_name}, your password is {encrypted_password} ans it is {len(password)} digits long.")
+
+
+# List in python
+
+amazon_cart = ["Notebooks", "Monitor", "Keyboard", "Mouse"]
+print(amazon_cart[0::2])
+
+#Adding
+amazon_cart.append("IPhone")
+print(amazon_cart)
+
+#Removing
+
+amazon_cart.remove("IPhone")
+print(amazon_cart)
+
+#Indexing
+
+
+print("Monitor" in amazon_cart)
+amazon_cart.sort()
+print(amazon_cart)
+
+
+
---
+++ python_basic_I/developer_fundamental_III/main.py
@@ -0,0 +1,8 @@
+# Dictionaries
+
+dictionary = {
+    'apple': 4,
+    'banana': 10,
+}
+
+print(dictionary['apple'])
