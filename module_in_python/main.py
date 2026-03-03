e2d82eedbb087a4ee276df8b5f23b8641808719b
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-03-03 09:49:22 -0500

    complete the module in python section

--- module_in_python/main.py
+++ module_in_python/main.py
@@ -1,7 +1,7 @@
-# import random
-#
-# print(random.random())
-# print(random.choifce([1,2,3,4,5,6,7,8,9,10]))
+import random
+
+print(random.random())
+print(random.choifce([1,2,3,4,5,6,7,8,9,10]))
 
 import random
 
@@ -26,3 +26,23 @@ while True:
     user_attempt -= 1
     if user_attempt == 0:
         break
+
+
+import pyjokes
+joke = pyjokes.get_joke(language='en')
+print(joke)
+
+#useful modules
+
+from collections import Counter, OrderedDict, defaultdict
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 2, 3,2,22,22]
+print(Counter(list))
+
+dictionary = defaultdict(lambda: "does not exist", {"a": "a", "b": "b", "c": "c"})
+print(dictionary["d"])
+
+
+# Date Time module
+from datetime import datetime
+print(datetime.now())
