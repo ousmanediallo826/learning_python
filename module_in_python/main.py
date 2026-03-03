fec38b58eb3b4e25b872cffc96418bb719b0381c
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2026-03-03 09:10:56 -0500

    build a guessing game for practice on the random module

--- module_in_python/main.py
+++ module_in_python/main.py
@@ -1,9 +1,28 @@
-# Learning module in python
+# import random
+#
+# print(random.random())
+# print(random.choifce([1,2,3,4,5,6,7,8,9,10]))
 
-import utility
-from shopping.shopping_cart import buy
+import random
 
 
-print(utility)
-print(buy("Apple"))
-print(buy("Mango"))
+
+
+user_attempt = 10
+while True:
+    computer = random.randint(1, 10)
+    user_input = int(input("Enter a number: "))
+    if computer > user_input:
+        print("Your guess is to low")
+
+    elif computer < user_input:
+        print("Your guess is to high")
+    elif computer == user_input:
+        print("You guessed it correctly")
+        break
+    else:
+        print("Please enter a number between 1 and 10")
+        continue
+    user_attempt -= 1
+    if user_attempt == 0:
+        break
